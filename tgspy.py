16180bf4d101ef6d9a2bcc74d1ec83ba0bc3e0d4
Hp <nurpratama001@gmail.com>
2021-10-26 08:55:03 -0700

    menambahkan tgspy.py

---
+++ tgspy.py
@@ -0,0 +1,20 @@
+##Input Data##
+
+nama = "Nur Pratama."
+panggilan = "Nur."
+nim ="312110305."
+lahir = "Bekasi."
+umur = "18"
+alamat = "Perum. Kota Serang Baru Blok C.4 No.2."
+telepon = "083818101568"
+
+##Output Data##
+
+print ("Assalammu'alaikum..")
+
+print ()
+
+print ("Let me introduce my self. My name is " +nama,"But you can call me " +panggilan,"My NIM is " +nim,"I was born in " + lahir,"and I am "+umur,"years old.","I am very glad if you want to invite my house in " +alamat,"So don't forget to call me before with the number " +telepon)
+
+print ()
+print ("Thank you.")
